d3bfecc854dce076e64ee37f928770839168f96c
Oluwadamilola Falodun <dpaulzs11@gmail.com>
2024-03-22 13:31:51 +0100

    check

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -25,6 +26,7 @@ const Footer = () => {
                     <li className="cursor-pointer hover:text-limeGreen transition-all">About Us</li>
                     <li className="cursor-pointer hover:text-limeGreen transition-all">Contact</li>
                     <li className="cursor-pointer hover:text-limeGreen transition-all">Blog</li>
+                    <Link to="/test">Test</Link>
                 </ul>
                 <ul className="flex flex-col gap-3">
                     <li className="cursor-pointer hover:text-limeGreen transition-all">Careers</li>
